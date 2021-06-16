0c4fec5ae4ba0fab224a1892bcaf2670a166d275
1chiSensei <mail@1chi.tk>
2021-06-16 13:56:32 +0800

    e

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useState } from 'react';
+import axios from 'axios';
 import './App.css';
 
+const getAdvice = async () => {
+  const { data } = await axios.get('https://api.adviceslip.com/advice');
+
+  return data.slip.advice;
+}
+
 function App() {
+  const [advice, setAdvice] = useState('');
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>{advice}</p>
+      <button onClick={async () => setAdvice(await getAdvice())}>
+        Click Me!
+      </button>
     </div>
   );
 }
